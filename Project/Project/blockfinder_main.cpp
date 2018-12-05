db6c42ac613bb69568c69b54808b3d61a0f67e69
Камилла Файзуллина <kamilla.faizullina@yandex.ru>
2018-12-05 18:21:12 +0300

    -

--- Project/Project/blockfinder_main.cpp
+++ Project/Project/blockfinder_main.cpp
@@ -40,28 +40,32 @@ int main(int argc, char *argv[]) {
 		convertend >> end; 
 
 		cout << "readed" << endl;
-	}
 
-	NCS test_nc2 = get_NCS(name_ncs);
+		NCS test_nc2 = get_NCS(name_ncs);
 
 
-	for (labeltype type_1 : test_nc2.label_types)
-		cout << setw(1) << type_1.name << " ";
-	cout << endl;
-	for (labeltype type_1 : test_nc2.label_types) {
-		cout << setw(1) << type_1.name << " ";
-		for (labeltype type_2 : test_nc2.label_types) {
-			cout << test_nc2.codes_dict[type_1][type_2] << " ";
-		};
+		for (labeltype type_1 : test_nc2.label_types)
+			cout << setw(1) << type_1.name << " ";
 		cout << endl;
+		for (labeltype type_1 : test_nc2.label_types) {
+			cout << setw(1) << type_1.name << " ";
+			for (labeltype type_2 : test_nc2.label_types) {
+				cout << test_nc2.codes_dict[type_1][type_2] << " ";
+			};
+			cout << endl;
+		}
+		cout << " vectors: " << endl;
+		cout << setw(7) << "Code ";
+		for (spectrum s : test_nc2.spec_list) {
+			//cout << setw(7) << s.name << " ";
+			cout << s.name << " ";
+		}
+		cout << endl;
+
+
 	}
-	cout << " vectors: " << endl;
-	cout << setw(7) << "Code ";
-	for (spectrum s : test_nc2.spec_list) {
-		//cout << setw(7) << s.name << " ";
-		cout << s.name << " ";
-	}
-	cout << endl;
+
+	
 
 
 	getchar();
